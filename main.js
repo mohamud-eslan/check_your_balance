435a6d92b471f18dbaa2070711f8757e013089a3
unknown <mohmud141@gmail.com>
2021-09-25 13:20:15 +0300

    added a html file

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-document.getElementById("submit").onclick = ()=>{
-    const salary = document.getElementById("salary").value;
-    const fuel = document.getElementById("fuel").value;
-    const food = document.getElementById("food").value;
-    const rent = document.getElementById("rent").value;
-    const entertainment = document.getElementById("entertainment").value;
 
-    const result = Number(salary)-Number(rent)-Number(food)-Number(fuel)-Number(entertainment);
-    const tax = 10/100;
-    const subTax = Number(salary) * tax;
-    document.getElementById("output").innerHTML = `hi mohamud your balance is ${result} and your tax is ${subTax.toFixed(2)}`};
+document.getElementById("send").addEventListener("click", calculate);
+function calculate(){
+    const car = document.getElementById("car").value;
+    const distance = document.getElementById("distance").value;
+    const output = document.getElementById("output");
+    
+    let cars;
+    if(cars === 50 && cajfkj)
+
+}
